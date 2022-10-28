2d906694ef8d9cbcfd927b129b7e3594c053b051
charles0830 <chberry830@gmail.com>
2022-10-28 07:16:49 +0000

    updated pakege

--- next.config.js
+++ next.config.js
@@ -1,35 +1,48 @@
-/* eslint-env node */
+// /* eslint-env node */
 
-// https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
-const nextConfig = {
-  webpack: config => {
-    const oneOfRule = config.module.rules.find(rule => rule.oneOf);
+// // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
+// const nextConfig = {
+//   webpack: config => {
+//     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
-    // Next 12 has multiple TS loaders, and we need to update all of them.
-    const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
+//     // Next 12 has multiple TS loaders, and we need to update all of them.
+//     const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
 
-    tsRules.forEach(rule => {
-      // eslint-disable-next-line no-param-reassign
-      rule.include = undefined;
-    });
+//     tsRules.forEach(rule => {
+//       // eslint-disable-next-line no-param-reassign
+//       rule.include = undefined;
+//     });
 
-    return config;
-  },
-  compress: true,
-  generateEtags: true,
-  pageExtensions: ['tsx', 'mdx', 'ts'],
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-  svgo: {
-    multipass: true,
-    plugins: ['removeDimensions'],
-  },
-  strictMode: true,
-  swcMinify: true,
-  trailingSlash: false,
+//     return config;
+//   },
+//   compress: true,
+//   generateEtags: true,
+//   pageExtensions: ['tsx', 'mdx', 'ts'],
+//   poweredByHeader: false,
+//   productionBrowserSourceMaps: false,
+//   svgo: {
+//     multipass: true,
+//     plugins: ['removeDimensions'],
+//   },
+//   strictMode: true,
+//   swcMinify: true,
+//   trailingSlash: false,
+//   images: {
+//     domains: ['images.unsplash.com', 'source.unsplash.com'],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/**
+   * @type {import('next').NextConfig}
+   */
+ const nextConfig   = {
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'],
+    loader: 'akamai',
+    path: '',
   },
+  assetPrefix: './',
 };
 
-module.exports = nextConfig;
+export default   nextConfig;
